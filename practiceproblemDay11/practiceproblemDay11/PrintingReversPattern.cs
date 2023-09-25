e73a1070b86e7a7b141610e14de600b9d08113a4
Manoj naik <chittinaik@gamil.com>
2023-09-25 20:54:36 +0530

    [manoj] add:Revers Triangle Useing Patter

---
+++ practiceproblemDay11/practiceproblemDay11/PrintingReversPattern.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practiceproblemDay11
+{
+    internal class PrintingReversPattern
+    {
+        public void Printing()
+        {
+            Console.WriteLine("Enter The Number:");
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = n; i >= 1; i--)
+            {
+                for (int j = 1; j <= n - i; j++)
+                {
+                    Console.WriteLine("");
+                }
+
+                for (int k = 1; k <= 2 * i - 1; k++)
+                {
+                    Console.Write("*");
+                }
+            }
+        }
+    }
+}
---
+++ practiceproblemDay11/practiceproblemDay11/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace practiceproblemDay11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Problem Number");
+            Console.WriteLine("1. Printing Rever triangle" +
+                "0. Exit");
+            int m=int.Parse(Console.ReadLine());
+            if(m==0)
+            {
+                Console.WriteLine("Enter The Valid Input");
+                return;
+            }
+            else
+            {
+                switch(m)
+                {
+                    case 1:
+                        PrintingReversPattern p=new PrintingReversPattern();
+                        p.Printing();
+                        break;
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+}
